b6979d8f77014c472d46dec3d5b012aa4ce42230
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-02-14 17:29:41 +0100

    docs: add CheckGuestForLoop class for bonus

--- java-festa-ferragnez/src/com/ferragnez/party/CheckGuestForLoop.java
+++ java-festa-ferragnez/src/com/ferragnez/party/CheckGuestForLoop.java
@@ -1,27 +1,30 @@
+// Bonus
+// se avete usato il ciclo for per cercare il nome nella lista nella consegna base, implementare lo stesso programma usando il ciclo while, e viceversa
+
 package com.ferragnez.party;
 
 import java.util.Scanner;
 
-public class CheckGuest {
+public class CheckGuestForLoop {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         // creazione array
-        String[] invitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        String[] invitati = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
 
         // richiesta nome utente
         System.out.println("Scrivi il tuo nome qui: ");
         String nome = input.nextLine();
 
-        // Ricerca nell'array con ciclo while
+        // ricerca nell'array con ciclo for
         boolean isOnList = false;
-        int i = 0;
 
-        while (!isOnList && i < invitati.length) {
+        for (int i = 0; i < invitati.length; i++) {
             String invitato = invitati[i];
 
             if (nome.equals(invitato)) {
                 isOnList = true;
+                break;
             }
             else {
                 i++;
@@ -36,8 +39,5 @@ public class CheckGuest {
         }
 
         input.close();
-
-        // PROBLEMI: come rendere l'input case insensitive? Fin quanto posso sbagliare una stringa?
-        // Esiste una formattazione per rendere l'input case insensitive, o una regex?
     }
 }
